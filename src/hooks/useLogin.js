75053411bbbc7ff81ba5c9c3516cba841612ace0
meet0510 <meetunpatel.0510@gmail.com>
2022-08-26 21:29:47 +0530

    Contest Display

--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { projectAuth } from "../firebase/config";
+import { projectAuth,projectFirestore } from "../firebase/config";
 import { useAuthContext } from "./useAuthContext";
 
 export const useLogin = () => {
@@ -12,14 +12,14 @@ export const useLogin = () => {
     setError(null);
     setIsPending(true);
 
-    // sign the user out
     try {
       const res = await projectAuth.signInWithEmailAndPassword(email, password);
 
       //dispatch logout action
       dispatch({ type: "LOGIN", payload: res.user });
 
-      //update state
+      // update online status
+      await projectFirestore.collection("users").doc(res.user.uid).update({ online: true });
 
       if (!isCancelled) {
         setIsPending(false);
--- src/pages/home/homePages/solo/DisplayContest.js
+++ src/pages/home/homePages/solo/DisplayContest.js
@@ -1,14 +1,78 @@
 import { useCollection } from "../../../../hooks/useCollection";
 import { useAuthContext } from "../../../../hooks/useAuthContext";
+import "./DisplayContest.css";
+import { useEffect, useState } from "react";
 
 export default function DisplayContest() {
   const { user } = useAuthContext();
-  const { documents, error } = useCollection("users", [
+  const [problems, setProblems] = useState(null);
+  const [error, setError] = useState(null);
+  const { documents, error: bug } = useCollection("users", [
     "cfHandle",
     "==",
     user.displayName,
   ]);
 
-  console.log(documents);
-  return <div></div>;
+  useEffect(() => {
+    if (bug) {
+      setError(bug);
+    }
+
+    if (documents) {
+      setProblems(documents[0].liveSoloContest.contestProblems);
+    }
+  }, [documents, bug]);
+
+  return (
+    <div>
+      {error && <p className="error">{error}</p>}
+      {!problems && <p className="error">Pending!!</p>}
+      {problems && (
+        <table>
+          <tbody>
+            <tr>
+              <th>ContestID</th>
+              <th>Index</th>
+              <th>ProblemName</th>
+              <th>Status</th>
+              <th>Rating</th>
+            </tr>
+            {problems.map((problem) => (
+              <tr key={problem.id} className="problem">
+                <td>
+                  <a
+                    href={"https://codeforces.com/contest/" + problem.contestId}
+                    target="_blank"
+                  >
+                    {problem.contestId}
+                  </a>
+                </td>
+                <td>{problem.index}</td>
+                <td>
+                  <a
+                    href={
+                      "https://codeforces.com/contest/" +
+                      problem.contestId +
+                      "/problem/" +
+                      problem.index
+                    }
+                    target="_blank"
+                  >
+                    {problem.name}
+                  </a>
+                </td>
+                <td>
+                  <img
+                    src="https://cdn.codeforces.com/s/18439/images/icons/submit-22x22.png"
+                    alt="Submit"
+                  ></img>
+                </td>
+                <td>{problem.rating}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </div>
+  );
 }
